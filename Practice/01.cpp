96a818b1f61b12189699e50e37a46a7287df2c85
Yuvraj Singh <77912320+uv-goswami@users.noreply.github.com>
2024-03-24 18:36:48 +0530

    Add files via upload

---
+++ Practice/01.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+    {
+        char name[50];
+        cout<<"Enter your name: "<<endl;
+        cin>>name;
+
+        cout<<"Hello,"<<name; "!"<<endl;
+
+        return 0;
+    }
